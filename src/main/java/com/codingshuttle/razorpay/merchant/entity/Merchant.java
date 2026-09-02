459fc91017a4e5cc84cff09209d9550335ecff0a
Sourav Makur <souravmakur888@gmail.com>
2026-09-03 01:15:45 +0530

    second commit

---
+++ src/main/java/com/codingshuttle/razorpay/merchant/entity/Merchant.java
@@ -0,0 +1,60 @@
+package com.codingshuttle.razorpay.merchant.entity;
+
+import com.codingshuttle.razorpay.common.enums.BusinessType;
+import com.codingshuttle.razorpay.common.enums.MerchantStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "merchant")
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class Merchant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id; //UUID is a 16 digit long id
+
+    @Column(nullable = false, length = 200)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false , unique = true)
+    private String contactNumber;
+
+    @Column(length = 50)
+    @Enumerated(EnumType.STRING)
+    private BusinessType businessType;
+
+    @Column(length = 200)
+    private String businessname;
+
+    @Column(length = 200)
+    private String websiteUrl;
+
+    @Column(length = 200, nullable = false)
+    @Enumerated(EnumType.STRING)
+    private MerchantStatus status =  MerchantStatus.PENDING_KYC;
+
+    @Column(length = 20)
+    private String gstId;
+
+    @Column(length = 20)
+    private String panId;
+
+    @Column(length = 200)
+    private String settlementBankAccount;
+
+    @Column(length = 20)
+    private String settlementBankIfsc;
+
+    @Column(length = 200)
+    private String settlementAccountHolderName;
+}
--- src/main/java/com/codingshuttle/razorpay/payment/entity/PaymentTransitionLog.java
+++ src/main/java/com/codingshuttle/razorpay/payment/entity/PaymentTransitionLog.java
@@ -1,9 +1,19 @@
 package com.codingshuttle.razorpay.payment.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.util.UUID;
 
 @Entity
 @Table(name = "payment_transition_log")
 public class PaymentTransitionLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "payment_id" , nullable = false)
+    private Payment payment;
+
+
 }
