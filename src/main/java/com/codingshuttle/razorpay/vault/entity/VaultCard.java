d733fe9828fd0cf735cb9fdba7731439280f24d5
Sourav Makur <souravmakur888@gmail.com>
2026-09-03 15:05:01 +0530

    Vault Card added

---
+++ src/main/java/com/codingshuttle/razorpay/vault/entity/VaultCard.java
@@ -0,0 +1,41 @@
+package com.codingshuttle.razorpay.vault.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "vault_card")
+public class VaultCard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, length = 4)
+    private String lastFour;
+
+    @Column(nullable = false, length = 6)
+    private String bin;
+
+    @Column(nullable = false)
+    private byte[] encryptedPan;
+
+    @Column(nullable = false)
+    private byte[] encryptedDek;
+
+    @Column(nullable = false)
+    private String brand; //Visa , rupay
+
+    @Column(nullable = false)
+    private String expiryMonth;
+
+    @Column(nullable = false)
+    private String expiryYear;
+
+    @Column(nullable = false)
+    private String cardHolderName;
+
+    private LocalDateTime deletedAt;
+}
