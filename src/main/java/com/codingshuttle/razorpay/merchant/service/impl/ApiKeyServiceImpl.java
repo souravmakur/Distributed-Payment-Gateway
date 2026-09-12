18598c3e369e7227f551bcb96500a5cbf1489cf7
Sourav Makur <souravmakur888@gmail.com>
2026-09-12 18:59:37 +0530

    Adding Api Key request

--- src/main/java/com/codingshuttle/razorpay/merchant/service/impl/ApiKeyServiceImpl.java
+++ src/main/java/com/codingshuttle/razorpay/merchant/service/impl/ApiKeyServiceImpl.java
@@ -1,8 +1,22 @@
 package com.codingshuttle.razorpay.merchant.service.impl;
 
 import com.codingshuttle.razorpay.merchant.service.ApiKeyService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 
-@Res
+@RestController
+@RequestMapping("/v1/merchants/{merchantId}/api-keys")
+@RequiredArgsConstructor
 public class ApiKeyServiceImpl implements ApiKeyService {
+    private final ApiKeyService apiKeyService;
+
+    @PostMapping
+    public ResponseEntity<> create (@PathVariable UUID merchantId,
+                                    @Valid @RequestBody CreateApiKeyRequest request) {
+
+    }
 }
